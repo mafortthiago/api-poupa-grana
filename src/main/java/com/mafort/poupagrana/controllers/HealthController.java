7e66cbb05106a6a758cb66e50515c75695bf63f5
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2025-02-13 22:58:43 -0300

    fix: correct health controller name.

--- src/main/java/com/mafort/poupagrana/controllers/HealthController.java
+++ src/main/java/com/mafort/poupagrana/controllers/HealthController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
-public class HealthCheckController {
+public class HealthController {
 
     @GetMapping
     public String healthCheck() {
