3262bd57443d077b50730f6ebb288664cd859c43
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2025-02-13 22:49:08 -0300

    feat(health): add health check endpoint

--- src/main/java/com/mafort/poupagrana/security/SecurityConfigurations.java
+++ src/main/java/com/mafort/poupagrana/security/SecurityConfigurations.java
@@ -35,6 +35,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.HEAD, "health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
