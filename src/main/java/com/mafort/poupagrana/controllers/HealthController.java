cb34f899bb75ea5445f1876fce816a6293980d7c
mafortthiago <thmafort@gmail.com>
2025-02-14 11:13:28 -0300

    fix(HealthController): add package name

--- src/main/java/com/mafort/poupagrana/controllers/HealthController.java
+++ src/main/java/com/mafort/poupagrana/controllers/HealthController.java
@@ -1,3 +1,5 @@
+package com.mafort.poupagrana.controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/mafort/poupagrana/security/SecurityConfigurations.java
+++ src/main/java/com/mafort/poupagrana/security/SecurityConfigurations.java
@@ -37,6 +37,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                         .requestMatchers(HttpMethod.HEAD, "health").permitAll()
+                        .requestMatchers(HttpMethod.GET, "health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
